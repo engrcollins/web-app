c14370c5f73bc724cd22ec9e09db72b1da29dcdb
engrcollins <akinbamicollins@gmail.com>
2020-05-02 22:59:12 +0000

    fix cors

--- server.js
+++ server.js
@@ -5,19 +5,27 @@ const cors = require("cors");
 const app = express();
 
 
-var corsOptions = {
+/*var corsOptions = {
   origin: "http://localhost:8081"
-};
+};*/
+
+var allowedOrigins = ['http://localhost:3000',
+                      'https://engrcollins.github.io/'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(cors(corsOptions));
-
-/*app.use((req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });*/
-
-
 
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
