31eacbe74f9e799e03928b1318868d0a58c95596
engrcollins <akinbamicollins@gmail.com>
2020-05-02 16:47:49 +0000

    edit db.cofig.js

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "Passion007!",
-    DB: "testdb"
+    HOST: "us-cdbr-east-06.cleardb.net",
+    USER: "ba474d786b0697",
+    PASSWORD: "67fb24b6",
+    DB: "heroku_78d1b74860cf736"
   };
+  
+ // mysql --host=us-cdbr-east-06.cleardb.net --user=ba474d786b0697 --password=67fb24b6 --reconnect heroku_78d1b74860cf736
